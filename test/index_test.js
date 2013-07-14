595902d01a60736d420fe75b7182adf3b1c979c5
Timo Behrmann <timo.behrmann@gmail.com>
2013-07-14 07:49:54 +0200

    Test

--- test/index_test.js
+++ test/index_test.js
@@ -1,5 +1,10 @@
+var server = require('../lib/server');
+var should = require('should');
+
 describe('test', function () {
     it('test', function (done) {
+        server.should.not.be.null;
+
         done();
     });
 });
